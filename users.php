a76f794b97f0a2f90d8264ee4db254c5f92a07f7
bart <birkholz96@gmx.de>
2014-04-19 01:50:51 +0200

    users sorted in 2 groups

--- users.php
+++ users.php
@@ -7,7 +7,14 @@
 	check_login();
 	check_admin(); 
 
-	$data = UserManager::get_userdata($_SESSION["user"]);	
+	$data = UserManager::get_userdata($_SESSION["user"]);
+	
+	if(isset($_GET["group"])) {
+		$group = $_GET["group"];
+	}
+	else {
+		$group = "students";
+	}
 	
 ?>
 
@@ -23,10 +30,51 @@
 		<?php require("nav-bar.php") ?>
 		<div id="user-management" class="container">
 			<h1>Nutzerverwaltung</h1>
-			<form id="data_form" name="data" action="save.php"></form>
+			<form id="data_form" name="data" action="save.php?group?<?php echo $group; ?>"></form>
 			<div class="users">
 				<h2>Nutzer</h2>
-				<table class="table table-striped">
+                <ul class="nav nav-tabs">
+                	<li<?php if($group == "students"): ?> class="active"<?php endif; ?>><a href="users.php?group=students">Schüler</a></li>
+                    <li<?php if($group == "teacher"): ?> class="active"<?php endif; ?>><a href="users.php?group=teacher">Lehrer</a></li>
+                </ul>
+                <?php if($group == "teacher"): ?>
+                <table class="table table-striped">
+					<thead>
+						<th>Vorname</th>
+						<th>Nachname</th>
+						<th>Spitzname</th>
+						<th>Geburtsdatum</th>
+						<th>Geschlecht</th>
+						<th class="edit"></th>
+					</thead>
+					<tbody>
+					<?php
+						global $mysqli;
+						
+						$stmt = $mysqli->prepare("
+							SELECT users.id AS id, users.prename, users.lastname, users.nickname, users.birthday, users.female
+							FROM teacher
+							LEFT JOIN users ON teacher.uid = users.id
+							ORDER BY users.lastname
+						");
+						
+						$stmt->execute();
+						$stmt->bind_result($row["id"], $row["prename"], $row["lastname"], $row["nickname"], $row["birthday"], $row["female"]);
+						
+						while($stmt->fetch()): ?>
+						<tr>
+							<td><?php echo $row["prename"] ?></td>
+							<td><?php echo $row["lastname"] ?></td>
+							<td><?php echo $row["nickname"] ?></td>
+							<td><?php echo $row["birthday"] ?></td>
+							<td><?php echo $row["female"] ? "Weiblich" : "Männlich" ?></td>
+							<td class="edit"><a href="edit-user.php?user=<?php echo $row["id"] ?>"><span class="icon-pencil-squared"></span></a></td>
+						</tr>
+					<?php endwhile; ?>
+					</tbody>
+				</table>
+                <?php else: ?>
+                <table class="table table-striped">
 					<thead>
 						<th>Vorname</th>
 						<th>Nachname</th>
@@ -40,18 +88,21 @@
 					<tbody>
 					<?php
 						global $mysqli;
-						$res = $mysqli->query("
+						
+						$stmt = $mysqli->prepare("
 							SELECT users.id AS id, users.prename, users.lastname, users.nickname, users.birthday, users.female, tutorial.name, tutor.lastname AS tutor 
-							FROM users
+							FROM students
+							LEFT JOIN users ON students.uid = users.id
 							LEFT JOIN tutorial ON users.class = tutorial.id
 							LEFT JOIN teacher ON tutorial.tutor = teacher.id
 							LEFT JOIN users tutor ON teacher.uid = tutor.id
 							ORDER BY users.lastname
-						");						
+						");
 						
-					?>
-					<?php $i = 0; ?>
-					<?php while($row = $res->fetch_assoc()): ?>
+						$stmt->execute();
+						$stmt->bind_result($row["id"], $row["prename"], $row["lastname"], $row["nickname"], $row["birthday"], $row["female"], $row["name"], $row["tutor"]);	
+					
+						while($stmt->fetch()): ?>
 						<tr>
 							<td><?php echo $row["prename"] ?></td>
 							<td><?php echo $row["lastname"] ?></td>
@@ -62,10 +113,11 @@
 							<td><?php echo $row["tutor"] ?></td>
 							<td class="edit"><a href="edit-user.php?user=<?php echo $row["id"] ?>"><span class="icon-pencil-squared"></span></a></td>
 						</tr>
-					<?php $i++; ?>
 					<?php endwhile; ?>
 					</tbody>
 				</table>
+                <?php endif; ?>
+				
 			</div>
 						
 			<div class="buttons">
