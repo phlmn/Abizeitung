22af336873c10a91f9d71480ec253b2d381523c5
Philipp Mandler <info@philipp-mandler.com>
2014-04-21 15:25:30 +0200

    Tooltips added

--- dashboard.php
+++ dashboard.php
@@ -190,6 +190,7 @@
 			$(document).ready(function(){
 				change_bg_img('#photo-enrollment', '<?php echo $enrollment; ?>');
 				change_bg_img('#photo-current', '<?php echo $current; ?>');
+				$("div.common *").tooltip();
 			});
 		</script>
 	</head>
@@ -250,8 +251,8 @@
 					</div>
 					
 					<div class="col-sm-4">
-						<div id="photo-enrollment" class="photo">
-							<form action="upload.php" id="image-form-enrollment" enctype="multipart/form-data" ></form>
+						<div id="photo-enrollment" class="photo" data-toggle="tooltip" data-placement="bottom" title="Einschulungsfoto">
+							<form action="upload.php" id="image-form-enrollment" enctype="multipart/form-data"></form>
 		                    <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo return_ini_bytes(ini_get('upload_max_filesize')); ?>" />
 							<input id="photo-upload-enrollment" class="photo-upload" name="photo" type="file" form="image-form-enrollment" accept="image/x-png,image/jpeg" onchange="uploadImage(<?php echo $data["id"] ?>, 1, '#image-form-enrollment', '#photo-upload-state-enrollment', '#photo-enrollment')" />
 							<div class="upload">
@@ -267,8 +268,8 @@
 					</div>
 					
 					<div class="col-sm-4">
-		                <div id="photo-current" class="photo">
-		                	<form action="upload.php" id="image-form-current" enctype="multipart/form-data" ></form>
+		                <div id="photo-current" class="photo"  data-toggle="tooltip" data-placement="bottom" title="Aktuelles Foto">
+		                	<form action="upload.php" id="image-form-current" enctype="multipart/form-data"></form>
 		                    <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo return_ini_bytes(ini_get('upload_max_filesize')); ?>" />
 							<input id="photo-upload-current" class="photo-upload" name="photo" type="file" form="image-form-current" accept="image/x-png,image/jpeg" onchange="uploadImage(<?php echo $data["id"] ?>, 2, '#image-form-current', '#photo-upload-state-current', '#photo-current')" />
 		                	<div class="upload">
